df66955cad2b4b4c5ddb24e00f0fc297edcc7e89
JasonCarvalho-tech <jml123carvalho@gmail.com>
2020-03-15 14:36:21 +0530

    added colour switching functionality

--- stylisation/themeSwitcher.js
+++ stylisation/themeSwitcher.js
@@ -1,3 +1,13 @@
+document.documentElement.style.setProperty('--bg-col', '#0f0f0f');
+document.documentElement.style.setProperty('--accent-col', '#fff');
+
 document.querySelector('.theme-toggle-button').addEventListener('click', () => {
-    document.body.classList.toggle('dark')
+    document.body.classList.toggle('dark');
+    if (document.body.className === "dark") {
+      document.documentElement.style.setProperty('--bg-col', '#fff');
+      document.documentElement.style.setProperty('--accent-col', '#000000');
+    } else {
+      document.documentElement.style.setProperty('--bg-col', '#0f0f0f');
+      document.documentElement.style.setProperty('--accent-col', '#fff');
+    }
   })
