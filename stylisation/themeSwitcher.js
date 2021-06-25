3740c2c6a064fdc5cf6884a2f0bb0d40313261a9
Jason Carvalho <jml123carvalho@gmail.com>
2021-06-25 10:00:38 +0530

    changed to use local storage instead of cookies

--- stylisation/themeSwitcher.js
+++ stylisation/themeSwitcher.js
@@ -1,23 +1,9 @@
-function getCookie(cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
 function setDark() {
   document.body.style.setProperty("--bg-col", "#000");
   document.body.style.setProperty("--accent-col", "#fff");
 
   document.body.style.setProperty("--blue-txt-color", "aqua");
+  localStorage.setItem("jasonlk-theme", "dark");
 }
 
 function setLight(){
@@ -29,40 +15,33 @@ function setLight(){
   if (window.matchMedia("(max-width: 768px)").matches) {
     document.body.style.setProperty("--project-small-txt", "aliceblue");
   }
+  localStorage.setItem("jasonlk-theme", "light");
 }
 
-
-if (document.cookie.split(';').filter((item) => item.trim().startsWith('theme=')).length) {
-  console.log('The cookie "theme" exists (ES6)');
-  var theme = getCookie("theme");
-  if (theme === "dark") {
+if (typeof(Storage) !== "undefined") {
+  if (localStorage.getItem("jasonlk-theme") == null) {
     setDark();
-  } else if (theme === "light") {
+  } else if (localStorage.getItem("jasonlk-theme") == "light") {
     setLight();
+  } else {
+    setDark();
   }
 } else {
-  document.cookie = "theme=dark";
-  console.log(document.cookie);
-  setDark();
+  $(".theme-toggle-button").hide();
 }
 
 document.getElementsByClassName("theme-toggle-button")[0].addEventListener("click", function() {
-  if(getCookie("theme") === "dark") {
-    document.cookie = "theme=light";
+  if(localStorage.getItem("jasonlk-theme") === "dark") {
     setLight()
   } else {
-    document.cookie = "theme=dark";
     setDark();
   }
 });
 
 document.getElementsByClassName("theme-toggle-button")[1].addEventListener("click", function() {
-  console.log("working");
-  if(getCookie("theme") === "dark") {
-    document.cookie = "theme=light";
+  if(localStorage.getItem("jasonlk-theme") === "dark") {
     setLight()
   } else {
-    document.cookie = "theme=dark";
     setDark();
   }
 });
