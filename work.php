17de6a37107b00eba5db443d434409a44d9377c9
Jason Carvalho <jml123carvalho@gmail.com>
2021-07-18 20:00:27 +0530

    initial commit

---
+++ work.php
@@ -0,0 +1,77 @@
+<?php $title = "Work | Jason Carvalho | Software Engineer";
+include 'templates/header.php';?>
+<div class="whitespace"></div>
+
+<!--------------- hero section starts here --------------->
+<section id="hero" class="container">
+        <div class="hero-content">
+            <br><br>
+
+            <div class="row">
+                    <div class="col-lg-8">
+
+                        <h3 class="wow fadeInUp" data-wow-delay="1s">Work.</h3><br>
+                        <p class="wow fadeInUp" data-wow-delay="1.2s">Posted below are some of my work. Clicking a project will take you to a page with more details on the project.</p>
+
+                    </div>
+            </div>
+        </div>
+</section>
+
+<!--------------- hero section ends here --------------->
+
+<!--------------- projects section starts here --------------->
+<section id="projects" class="container-fluid">
+        <div class="whitespace"></div>
+
+        <div class="row">
+            <div class="col-lg-7"></div>
+
+            <div class="col-lg-3 project project5 wow fadeInUp" onclick="javascript:location.href='./projects/project-5.html'"></div>
+
+            <div class="col-lg-1"></div>
+        </div>
+
+        <div class="whitespace"></div>
+
+        <div class="row">
+            <div class="col-lg-0"></div>
+
+            <div class="col-lg-3 project project4 wow fadeInUp" onclick="javascript:location.href='./projects/project-4.html'"></div>
+
+            <div class="col-lg-6"></div>
+        </div>
+
+        <div class="whitespace"></div>
+
+        <div class="row">
+            <div class="col-lg-7"></div>
+
+            <div class="col-lg-4 project project3 wow fadeInUp" onclick="javascript:location.href='./projects/project-3.html'"></div>
+
+            <div class="col-lg-1"></div>
+        </div>
+
+        <div class="whitespace"></div>
+
+        <div class="row">
+            <div class="col-lg-0"></div>
+
+            <div class="col-lg-3 project project2 wow fadeInUp" onclick="javascript:location.href='./projects/project-2.html'"></div>
+
+            <div class="col-lg-6"></div>
+        </div>
+
+        <div class="whitespace"></div>
+
+        <div class="row">
+            <div class="col-lg-7"></div>
+
+            <div class="col-lg-4 project project1 wow fadeInUp" onclick="javascript:location.href='./projects/project-1.html'"></div>
+
+            <div class="col-lg-1"></div>
+        </div>
+</section>
+
+            <!--------------- projects section ends here --------------->
+<?php include 'templates/footer.php';?>
