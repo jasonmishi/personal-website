9657fb1993f9db75d292cf23436470be826b90ff
Jason Carvalho <jml123carvalho@gmail.com>
2021-07-18 22:19:57 +0530

    initial commit

---
+++ projects/project-3.php
@@ -0,0 +1,66 @@
+<?php $title = "Jason Carvalho | Software Engineer";
+        include '../templates/header.php';?>
+<div class="whitespace"></div>
+
+<!--------------- hero section starts here --------------->
+<div class="container">
+    <div class="hero-content">
+        <br><br>
+        <div class="row">
+            <div class="col-lg-12">
+                <br>
+
+                <h1 class="wow fadeInUp" data-wow-delay="1s">A* search algorithm (p5.js)</h1><br><br>
+
+                <div class="row">
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.2s">service :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.3s">Web Development</h6>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.4s">started :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.5s">20 Feb 2020</h6>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.6s">completed :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.7s">29 Feb 2020</h6>
+                    </div>
+                </div>
+                <br>
+                <p class="wow fadeInUp" data-wow-delay="1.8s">This is an implementation of the popular A* search algorithm in java script using the p5.js library. It uses several object oriented concepts. Choosing the fonts and designing the UI was also a lot of fun. I learnt a lot about the p5 library and got its basic principles down. The webpage can be found <a href="../A search/index.html">here</a>. The code is on this <a href="https://github.com/JasonCarvalho-tech/A-star-search-P5.js">github repository</a>.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!--------------- hero section ends here --------------->
+
+
+<!--------------- project image section starts here --------------->
+<div>
+    <video class="project-image" id="video1" poster="" preload autoplay loop muted>
+        <source src="project-3.mp4" type="video/mp4" />
+        Your browser does not support the video element.
+    </video>
+</div>
+<!--------------- project image section ends here --------------->
+
+<br><br>
+
+<!--------------- project nav starts here --------------->
+
+<div class="project-nav">
+    <div class="container">
+        <div class="row">
+            <span onclick="javascript:location.href='../projects/project-2.html'"><ion-icon name="arrow-back"></ion-icon>previous</span>
+            <span onclick="javascript:location.href='../projects/project-4.html'">next<ion-icon name="arrow-forward"></ion-icon></span>
+        </div>
+    </div>
+</div>
+
+<!--------------- project nav starts here --------------->
+
+<div class="whitespace"></div>
+
+<?php include '../templates/footer.php'?>
---
+++ projects/project-4.php
@@ -0,0 +1,63 @@
+<?php $title = "Jason Carvalho | Software Engineer";
+      include '../templates/header.php';?>
+<div class="whitespace"></div>
+
+<!--------------- hero section starts here --------------->
+<div class="container">
+    <div class="hero-content">
+        <br><br>
+        <div class="row">
+            <div class="col-lg-12">
+                <br>
+
+                <h1 class="wow fadeInUp" data-wow-delay="1s">Conway\'s Game Of Life</h1><br><br>
+
+                <div class="row">
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.2s">service :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.3s">Web Development(React)</h6>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.4s">started :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.5s">29 Feb 2020</h6>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.6s">completed :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.7s">6 Mar 2020</h6>
+                    </div>
+                </div>
+                <br>
+                <p class="wow fadeInUp" data-wow-delay="1.8s">This is an implementation of Conway\'s Game Of Life using React. Conway\'s game of life is a zero-player cellular automaton that has an infinite board. However my board is restricted to several sizes. Furthermore the website is fully responsive, but the grid does not render well on smaller screens. Really happy with the colour choices and the way it looks. Also thrilled that I could get the cells to be alive or dead using mouse clicks. The application can be found <a href="../GameOfLife/">here</a>. You can view the code on this <a href="https://github.com/JasonCarvalho-tech/Game-Of-Life-js">github repository</a>. To learn more about Conway\'s game of life you can go to its <a href="https://en.wikipedia.org/wiki/Conway's_Game_of_Life">Wikipedia page</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+<!--------------- hero section ends here --------------->
+
+
+<!--------------- project image section starts here --------------->
+<div>
+    <img class="project-image" src="./project-4.jpg" alt="Conway's game of Life">
+</div>
+<!--------------- project image section ends here --------------->
+
+<br><br>
+
+<!--------------- project nav starts here --------------->
+
+<div class="project-nav">
+    <div class="container">
+        <div class="row">
+            <span onclick="javascript:location.href='../projects/project-3.html'"><ion-icon name="arrow-back"></ion-icon>previous</span>
+            <span onclick="javascript:location.href='../projects/project-1.html'">next<ion-icon name="arrow-forward"></ion-icon></span>
+        </div>
+    </div>
+</div>
+
+<!--------------- project nav starts here --------------->
+
+<div class="whitespace"></div>
+
+<?php include '../templates/footer.php'?>
