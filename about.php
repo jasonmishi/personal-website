d285759a75cdd67da42fd161a2b5978cfa1356fe
Jason Carvalho <jml123carvalho@gmail.com>
2021-07-18 20:00:27 +0530

    initial commit

---
+++ about.php
@@ -0,0 +1,32 @@
+<?php $title = "About Me | Jason Carvalho | Software Engineer";
+include 'templates/header.php';?>
+<!--------------- hero image starts here --------------->
+<div class="container">
+            <div class="col-lg-12 about wow fadeInUp" data-wow-delay="1s">
+                    <img src="./stylisation/aboutImage.jpg">
+            </div>
+</div>
+<!--------------- hero image ends here --------------->
+
+
+<!--------------- hero section starts here --------------->
+<div class="container">
+        <div class="hero-content">
+            <br><br>
+
+            <div class="row">
+                    <div class="col-lg-12">
+                        <h3 class="wow fadeInUp">About me.</h3><br>
+
+                        <p class="wow fadeInUp" data-wow-delay="0.4s">As previously mentioned in the home page I'm a software engineer based in Colombo, Sri Lanka. I am familiar with a variety of programming laguages such as JavaScript, HTML, CSS, MYSQL, Delphi and at the same time I'm learning to write code in a bunch of other languages such as ASP and VB.net as well. Given the right information it is posiible for me to design a fully functional and responsive website that is customized with bootstrap and CSS. Drop me a message using the <a href="contact.htm">contact page</a> and we can discuss the specifics</p>
+
+                        <p class="wow fadeInUp" data-wow-delay="0.8s">If you are looking to hire me as a software engineer, you can download a copy of my CV. Please note that because I'm a student I won't be able to work on weekends</p>
+
+                    </div>
+            </div>
+        </div>
+</div>
+<!--------------- hero section ends here --------------->
+
+<div class="whitespace"></div>
+<?php include 'templates/footer.php' ?>
