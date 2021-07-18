87f75b750e155345ebf89d6ca221272074dd9aec
Jason Carvalho <jml123carvalho@gmail.com>
2021-07-18 22:19:57 +0530

    initial commit

---
+++ projects/project-2.php
@@ -0,0 +1,64 @@
+<?php $title = "Jason Carvalho | Software Engineer";
+        include '../templates/header.php';?>
+
+<div class="whitespace"></div>
+
+<!--------------- hero section starts here --------------->
+<div class="container">
+    <div class="hero-content">
+        <br><br>
+        <div class="row">
+            <div class="col-lg-12">
+                <br>
+
+                <h1 class="wow fadeInUp" data-wow-delay="1s" onclick="javascript:location.href='../holmes-homes/holmes-homes.html'">Holmes\' Homes (Real estate site)</h1><br><br>
+
+                <div class="row">
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.2s">service :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.3s">Web Development</h6>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.4s">started :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.5s">8 October 2019</h6>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.6s">completed :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.7s">18 October 2019</h6>
+                    </div>
+                </div>
+                <br>
+                <p class="wow fadeInUp" data-wow-delay="1.8s">This website was been written in HTML, CSS, JavaScript. I used a third party plugin called 'Slick Slider' for the sliders. This website is fully responsive and highlights my ablility to design a fully functional website using a variety of frameworks. I hope to further this project by making a property finder using the google maps API. The website can be found <a href="../holmes-homes/holmes-homes.html" target="_blank">here</a>. The GitHub repo can be found <a href="https://github.com/JasonCarvalho-tech/real-estate" target="_blank">here</a>.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!--------------- hero section ends here --------------->
+
+
+<!--------------- project image section starts here --------------->
+<div>
+    <img class="project-image" src="./project-2.jpg" alt="Image of personal website">
+</div>
+<!--------------- project image section ends here --------------->
+
+<br><br>
+
+<!--------------- project nav starts here --------------->
+
+<div class="project-nav">
+    <div class="container">
+        <div class="row">
+            <span onclick="javascript:location.href='../projects/project-1.html'"><ion-icon name="arrow-back"></ion-icon>previous</span>
+            <span onclick="javascript:location.href='../projects/project-3.html'">next<ion-icon name="arrow-forward"></ion-icon></span>
+        </div>
+    </div>
+</div>
+
+<!--------------- project nav starts here --------------->
+
+<div class="whitespace"></div>
+
+<?php include '../templates/footer.php'?>
---
+++ projects/project-5.php
@@ -0,0 +1,63 @@
+<?php $title = "Jason Carvalho | Software Engineer";
+      include '../templates/header.php';?>
+<div class="whitespace"></div>
+
+<!--------------- hero section starts here --------------->
+<div class="container">
+    <div class="hero-content">
+        <br><br>
+        <div class="row">
+            <div class="col-lg-12">
+                <br>
+
+                <h1 class="wow fadeInUp" data-wow-delay="1s">Maze Generator</h1><br><br>
+
+                <div class="row">
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.2s">service :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.3s">Web Development</h6>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.4s">started :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.5s">6 Mar 2020</h6>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <p class="wow fadeInUp" data-wow-delay="1.6s">completed :</p>
+                        <h6 class="wow fadeInUp" data-wow-delay="1.7s">10 Mar 2020</h6>
+                    </div>
+                </div>
+                <br>
+                <p class="wow fadeInUp" data-wow-delay="1.8s">A maze generator made using the p5.js library. It uses a backtracking algorithm.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!--------------- hero section ends here --------------->
+
+
+<!--------------- project image section starts here --------------->
+<div>
+    <img class="project-image" src="./project-4.jpg" alt="Conway's game of Life">
+</div>
+<!--------------- project image section ends here --------------->
+
+<br><br>
+
+<!--------------- project nav starts here --------------->
+
+<div class="project-nav">
+    <div class="container">
+        <div class="row">
+            <span onclick="javascript:location.href='../projects/project-3.html'"><ion-icon name="arrow-back"></ion-icon>previous</span>
+            <span onclick="javascript:location.href='../projects/project-1.html'">next<ion-icon name="arrow-forward"></ion-icon></span>
+        </div>
+    </div>
+</div>
+
+<!--------------- project nav starts here --------------->
+
+<div class="whitespace"></div>
+
+<?php include '../templates/footer.php'?>
