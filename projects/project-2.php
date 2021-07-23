6c2dbe22a4e5de08e20df6816df0eb937afc225f
Jason Carvalho <jml123carvalho@gmail.com>
2021-07-23 14:20:22 +0530

    changed project image sizing and positioning

--- projects/project-2.php
+++ projects/project-2.php
@@ -11,7 +11,7 @@
             <div class="col-lg-12">
                 <br>
 
-                <h1 class="wow fadeInUp" data-wow-delay="1s" onclick="javascript:location.href='../holmes-homes/holmes-homes.html'">Holmes\' Homes (Real estate site)</h1><br><br>
+                <h1 class="wow fadeInUp" data-wow-delay="1s" onclick="javascript:location.href='../holmes-homes/holmes-homes.html'">Holmes' Homes (Real estate site)</h1><br><br>
 
                 <div class="row">
                     <div class="col-lg-4">
@@ -33,16 +33,16 @@
                 <p class="wow fadeInUp" data-wow-delay="1.8s">This website was been written in HTML, CSS, JavaScript. I used a third party plugin called 'Slick Slider' for the sliders. This website is fully responsive and highlights my ablility to design a fully functional website using a variety of frameworks. I hope to further this project by making a property finder using the google maps API. The website can be found <a href="../holmes-homes/holmes-homes.html" target="_blank">here</a>. The GitHub repo can be found <a href="https://github.com/JasonCarvalho-tech/real-estate" target="_blank">here</a>.</p>
             </div>
         </div>
+        <!--------------- project image section starts here --------------->
+        <div>
+            <img class="project-image wow fadeInUp" data-wow-delay="2s" src="./project-2.jpg" alt="Image of personal website">
+        </div>
+        <!--------------- project image section ends here --------------->
     </div>
 </div>
 <!--------------- hero section ends here --------------->
 
 
-<!--------------- project image section starts here --------------->
-<div>
-    <img class="project-image" src="./project-2.jpg" alt="Image of personal website">
-</div>
-<!--------------- project image section ends here --------------->
 
 <br><br>
 
@@ -51,8 +51,8 @@
 <div class="project-nav">
     <div class="container">
         <div class="row">
-            <span onclick="javascript:location.href='../projects/project-1.html'"><ion-icon name="arrow-back"></ion-icon>previous</span>
-            <span onclick="javascript:location.href='../projects/project-3.html'">next<ion-icon name="arrow-forward"></ion-icon></span>
+            <span onclick="javascript:location.href='../projects/project-1.php'"><ion-icon name="arrow-back"></ion-icon>previous</span>
+            <span onclick="javascript:location.href='../projects/project-3.php'">next<ion-icon name="arrow-forward"></ion-icon></span>
         </div>
     </div>
 </div>
--- projects/project-4.php
+++ projects/project-4.php
@@ -10,7 +10,7 @@
             <div class="col-lg-12">
                 <br>
 
-                <h1 class="wow fadeInUp" data-wow-delay="1s">Conway\'s Game Of Life</h1><br><br>
+                <h1 class="wow fadeInUp" data-wow-delay="1s">Conway's Game Of Life</h1><br><br>
 
                 <div class="row">
                     <div class="col-lg-4">
@@ -29,19 +29,17 @@
                     </div>
                 </div>
                 <br>
-                <p class="wow fadeInUp" data-wow-delay="1.8s">This is an implementation of Conway\'s Game Of Life using React. Conway\'s game of life is a zero-player cellular automaton that has an infinite board. However my board is restricted to several sizes. Furthermore the website is fully responsive, but the grid does not render well on smaller screens. Really happy with the colour choices and the way it looks. Also thrilled that I could get the cells to be alive or dead using mouse clicks. The application can be found <a href="../GameOfLife/">here</a>. You can view the code on this <a href="https://github.com/JasonCarvalho-tech/Game-Of-Life-js">github repository</a>. To learn more about Conway\'s game of life you can go to its <a href="https://en.wikipedia.org/wiki/Conway's_Game_of_Life">Wikipedia page</a></p>
+                <p class="wow fadeInUp" data-wow-delay="1.8s">This is an implementation of Conway's Game Of Life using React. Conway's game of life is a zero-player cellular automaton that has an infinite board. However my board is restricted to several sizes. Furthermore the website is fully responsive, but the grid does not render well on smaller screens. Really happy with the colour choices and the way it looks. Also thrilled that I could get the cells to be alive or dead using mouse clicks. The application can be found <a href="../GameOfLife/">here</a>. You can view the code on this <a href="https://github.com/JasonCarvalho-tech/Game-Of-Life-js">github repository</a>. To learn more about Conway's game of life you can go to its <a href="https://en.wikipedia.org/wiki/Conway's_Game_of_Life">Wikipedia page</a></p>
             </div>
         </div>
+        <!--------------- project image section starts here --------------->
+        <div>
+            <img class="project-image wow fadeInUp" data-wow-delay="2s" src="./project-4.jpg" alt="Conway's game of Life">
+        </div>
+        <!--------------- project image section ends here --------------->
     </div>
 </div>
 <!--------------- hero section ends here --------------->
-
-
-<!--------------- project image section starts here --------------->
-<div>
-    <img class="project-image" src="./project-4.jpg" alt="Conway's game of Life">
-</div>
-<!--------------- project image section ends here --------------->
 
 <br><br>
 
@@ -50,8 +48,8 @@
 <div class="project-nav">
     <div class="container">
         <div class="row">
-            <span onclick="javascript:location.href='../projects/project-3.html'"><ion-icon name="arrow-back"></ion-icon>previous</span>
-            <span onclick="javascript:location.href='../projects/project-1.html'">next<ion-icon name="arrow-forward"></ion-icon></span>
+            <span onclick="javascript:location.href='../projects/project-3.php'"><ion-icon name="arrow-back"></ion-icon>previous</span>
+            <span onclick="javascript:location.href='../projects/project-5.php'">next<ion-icon name="arrow-forward"></ion-icon></span>
         </div>
     </div>
 </div>
