80170f9be23092299342962831aaec90258a1c2a
Jason Mishike <jasonmishi@gmail.com>
2023-07-30 22:39:04 +0530

    add bootstrap-icons.svg to viteStaticCopy config
    so it can be accessed as sprites from the html code.

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,16 @@
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 export default {
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: '../node_modules/bootstrap-icons/bootstrap-icons.svg',
+          dest: '/'
+        }
+      ]
+    })
+  ],
   root: 'src',
   build: {
     outDir: '../dist'
