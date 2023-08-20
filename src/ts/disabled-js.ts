2745e6b08bf23e8610265b5c268a9310851fc620
Jason Mishike <jasonmishi@gmail.com>
2023-08-20 21:48:05 +0530

    add get_em_to_px_rate() for unit conversion

--- src/ts/disabled-js.ts
+++ src/ts/disabled-js.ts
@@ -1,6 +1,6 @@
 /**
- * Use this file to hide certain elements ect instead of using css so if the
+ * Use this file to hide/show certain elements ect instead of using css so if the
  * user has js disabled they will still be able to use the site
  */
 
-console.log('js enabled')
+const get_em_to_px_rate = () => document.querySelector('#em')?.clientWidth || 16;
