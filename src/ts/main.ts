829a76a6278bedc06fb0d4c4d14fc7aa7b6636eb
Jason Mishike <jasonmishi@gmail.com>
2023-08-20 21:46:53 +0530

    linting

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,6 @@
+"use strict";
+
+// eslint-disable-next-line no-useless-escape
 const asciiArt = '   __  ______  ______  ______  __   __  __    __  __  ______  __  __  __    \r\n  \/\\ \\\/\\  __ \\\/\\  ___\\\/\\  __ \\\/\\ \"-.\\ \\\/\\ \"-.\/  \\\/\\ \\\/\\  ___\\\/\\ \\_\\ \\\/\\ \\   \r\n _\\_\\ \\ \\  __ \\ \\___  \\ \\ \\\/\\ \\ \\ \\-.  \\ \\ \\-.\/\\ \\ \\ \\ \\___  \\ \\  __ \\ \\ \\  \r\n\/\\_____\\ \\_\\ \\_\\\/\\_____\\ \\_____\\ \\_\\\\\"\\_\\ \\_\\ \\ \\_\\ \\_\\\/\\_____\\ \\_\\ \\_\\ \\_\\ \r\n\\\/_____\/\\\/_\/\\\/_\/\\\/_____\/\\\/_____\/\\\/_\/ \\\/_\/\\\/_\/  \\\/_\/\\\/_\/\\\/_____\/\\\/_\/\\\/_\/\\\/_\/ ';
 console.log(asciiArt);
 
@@ -6,7 +9,7 @@ console.log(asciiArt);
  */
 const main = document.querySelector('main');
 const resizeObserver = new ResizeObserver(entries => {
-  for (let entry of entries) {
+  for (const entry of entries) {
     const navbarHeight = entry.contentRect.height;
     if (main) main.style.paddingTop = `${navbarHeight}px`;
   }
