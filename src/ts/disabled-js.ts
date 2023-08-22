280b8a34db6200c313d988e06e735fc058586ef2
Jason Mishike <jasonmishi@gmail.com>
2023-08-22 21:46:39 +0530

    add breakpoints calculation using js

--- src/ts/disabled-js.ts
+++ src/ts/disabled-js.ts
@@ -5,4 +5,27 @@
 
 document.querySelectorAll('.js-required').forEach((el) => el.classList.remove('js-required'));
 
-const get_em_to_px_rate = () => document.querySelector('#em')?.clientWidth || 16;
+const em_reference_element = document.querySelector('#em');
+const get_em_to_px_rate = () => em_reference_element?.clientWidth || 16;
+
+em_reference_element
+
+const break_points_em = {
+  sm: 40,
+  md: 48,
+  lg: 64,
+  xl: 80,
+}
+const break_points: {[key: string]: number} = {};
+
+const set_break_points = new ResizeObserver(() => {
+  const em_to_px_rate = get_em_to_px_rate();
+  for(const [key, value] of Object.entries(break_points_em)) {
+    break_points[key] = value * em_to_px_rate;
+  }
+});
+
+if (em_reference_element)
+  set_break_points.observe(em_reference_element);
+else
+  console.error('em_reference_element not found');
