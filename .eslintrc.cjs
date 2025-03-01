3ad4fada478350be7e954c743c7876a340525fd4
Jason Mishike <jasonmishi@gmail.com>
2025-03-01 15:29:12 +0530

    Fix cloudflare wrangler command (#9)
    * fix cloudflare wrangler command
    
    include the branch name and the commit sha
    
    * linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,8 @@
 /* eslint-env node */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
